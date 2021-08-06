3e63c919f8a6d3d0cdfb2623f4b46b4fe9551740
ApurvaSinghLko <88497948+ApurvaSinghLko@users.noreply.github.com>
2021-08-06 08:52:22 +0530

    Welcome To My Repository
    This Is Source Code of CodeWithHarry Video https://www.youtube.com/watch?v=SuLiu5AK9Ps
    Because Content Creater Has Not Yet Uploaded His Work Against His Promise In His Video
    This Code Was Written In Visual Studio 2019. Most Of Code Is Commented Out So That Learner Can UnComment It Whenever He Needs It.
    Please Compile Code If You Are Using Visual Studio
    The Code Was Made To Be Used In Online Compiler
    Thanks

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * this is multiple line comment
+*/
+// this is a single line comment
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data type Examples
+            /*int a = 34;
+            float b = 52.33F;
+            double c = 29.15D;
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+            Console.WriteLine(c);
+            bool igreat = true;
+            char e = 'R';
+            string hg = "This is a strung bro";
+            Console.WriteLine(igreat);
+            Console.WriteLine(e);
+            Console.WriteLine(hg);
+            */
+
+
+            //print examples
+            /*int example = 123456789;
+            string inp = Console.ReadLine();
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine("I Love C#" + inp);
+            Console.Write("india");
+            Console.WriteLine("Heloo India" + example);
+            Console.ReadLine();
+            */
+
+            //Type Casting
+            /*   there are two types of castings/Change Varibles   
+             *    1. Implicit Casting
+             *    2. Explicit Casting
+             * 
+             * 
+             *    1. Implicit Casting
+             *    Variable Can be Promoted by only this order
+             *    Char to int to long to float to double
+             *    It is done automatically like Amazon Home Delevery
+             * 
+             *
+             *
+             * 2.Explicit Casting
+             * 
+             * it is done manually like buying things from shop.
+             * int X = (int) = 3.5
+             * 
+            //implicit casting examples
+            double hy = (double)3.5;
+            Console.WriteLine(hy);
+            */
+            /*
+            //Explicit Casting Examples
+            //int t = (int) 5.33;
+            //Console.WriteLine(t)
+            int x = 5;
+            double y = x;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            //string inp = Console.ReadLine();
+            //Console.WriteLine(inp);
+
+            char h = 'y';
+            int j = h;
+            Console.WriteLine(j);
+            Console.WriteLine(h);
+
+            */
+            // other methods of typecasting
+            // built in methods
+            //float varr = Convert.ToInt32(3.224);
+            //Console.WriteLine(varr);
+            
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
